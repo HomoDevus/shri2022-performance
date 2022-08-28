4b8e43ba7e984726320be59dc5c28b8adc83d8e7
ItisEternity <lavrovslava207@gmail.com>
2022-08-28 19:11:35 +0300

    images optimization

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,35 @@
 import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
-// import { esbuildCommonjs, viteCommonjs } from '@originjs/vite-plugin-commonjs'
-import requireTransform from 'vite-plugin-require-transform';
+import viteImagemin from 'vite-plugin-imagemin'
 
 export default defineConfig({
   plugins: [
-    // passing string type Regular expression
-    requireTransform({}),
+    viteImagemin({
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false,
+      },
+      optipng: {
+        optimizationLevel: 7,
+      },
+      mozjpeg: {
+        quality: 20,
+      },
+      pngquant: {
+        quality: [0.8, 0.9],
+        speed: 4,
+      },
+      svgo: {
+        plugins: [
+          {
+            name: 'removeViewBox',
+          },
+          {
+            name: 'removeEmptyAttrs',
+            active: false,
+          },
+        ],
+      },
+    }),
   ],
   base: ''
 })
