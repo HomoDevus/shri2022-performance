bae1db48942987d6b19a4c93e70d9c7599191cfe
ItisEternity <lavrovslava207@gmail.com>
2022-08-29 00:54:46 +0300

    resource loading optimization

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,11 @@
 import { defineConfig } from 'vite'
 import viteImagemin from 'vite-plugin-imagemin'
+import { imagetools } from 'vite-imagetools'
 
 export default defineConfig({
   plugins: [
-    viteImagemin({
-      gifsicle: {
-        optimizationLevel: 7,
-        interlaced: false,
-      },
-      optipng: {
-        optimizationLevel: 7,
-      },
-      mozjpeg: {
-        quality: 20,
-      },
-      pngquant: {
-        quality: [0.8, 0.9],
-        speed: 4,
-      },
-      svgo: {
-        plugins: [
-          {
-            name: 'removeViewBox',
-          },
-          {
-            name: 'removeEmptyAttrs',
-            active: false,
-          },
-        ],
-      },
-    }),
+    viteImagemin(),
+    imagetools()
   ],
   base: ''
 })
